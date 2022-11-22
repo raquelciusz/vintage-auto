3574055434d9b6bed220e83c6deacd5f0da38dd9
Raquel Ciusz <raquel.ciusz@yahoo.com>
2022-11-22 16:06:01 -0300

    show updated with car info

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,7 +32,7 @@ puts 'Creating cars'
     price: (30000..100000).to_a.sample,
     location: location.sample,
     description: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    user_id:
+    user:
   );
   puts "car #{car.id} is created."
 end
