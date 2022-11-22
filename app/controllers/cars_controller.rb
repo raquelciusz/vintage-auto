1a0f7f8faab39219d5fa7c75e62b51cc0dbca59e
Raquel Ciusz <raquel.ciusz@yahoo.com>
2022-11-22 08:55:11 -0300

    car controller done, need to test now

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,22 +1,48 @@
 class CarsController < ApplicationController
+  before_action :set_list, only: %i[ show edit update destroy ]
+
   def index
+    @cars = Car.all
   end
 
   def show
   end
 
   def new
+    @car = Car.new
   end
 
   def update
+    if @car.update!(car_params)
+      redirect_to @car
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @car.destroy
+    redirect_to cars_path, notice: "Car was successfully destroyed."
   end
 
   def create
+    @car = Car.create(car_params)
+    if @car.save
+      redirect_to car_path(@car)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+  end
+
+  private
+  def set_car
+    @car = Car.find(params[:id])
+  end
+
+  def car_params
+    params.require(:car).permit(:brand, :model, :year, :km, :color, :type, :price, :location, :avaiable, :description, :user_id)
   end
 end
