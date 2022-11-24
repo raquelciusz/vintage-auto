b67581b100f2a5624e3010ba98d940e9f32a4c4e
Renoir Lucena <renoirlucena@gmail.com>
2022-11-24 13:45:33 -0300

    search is done

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,16 @@
 class Car < ApplicationRecord
+  include PgSearch::Model
   belongs_to :user
   has_many :sales, dependent: :destroy
-
   has_many_attached :photo_url
+
+  pg_search_scope :search,
+  against: [
+    :brand, :model,
+    :year, :color,
+    :location, :description
+  ],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
